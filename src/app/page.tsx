c4009f1027353d2347c2fbbd1deb9c70670bcd70
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2025-03-18 16:31:37 -0700

    add overview section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/landing/hero";
+import { Overview } from "@/components/landing/overview";
 
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center">
       <Hero />
+      <Overview />
     </main>
   );
 }
---
+++ src/components/landing/overview.tsx
@@ -0,0 +1,31 @@
+import { Separator } from "@/components/ui/separator";
+
+export function Overview() {
+  return (
+    <div className="w-full px-16">
+      <div className="bg-muted flex w-full flex-col items-center justify-center gap-4 rounded-3xl py-4">
+        <p className="text-muted-foreground opacity-50">
+          Eliminate the confusion all while saving time.
+        </p>
+        <Separator className="opacity-60" />
+        <div className="flex h-8 items-center justify-center gap-16">
+          <p>Instant AI Insights</p>
+          <Separator orientation="vertical" className="opacity-70" />
+          <p>Clarity in Data</p>
+          <Separator orientation="vertical" className="opacity-70" />
+          <p>Real-time Alerts</p>
+          <Separator orientation="vertical" className="opacity-70" />
+          <p>Quick Key Takeaways</p>
+        </div>
+        <p className="text-muted-foreground p-12 text-center text-lg leading-tight opacity-50">
+          <span className="font-bold">The problem?</span> The everyday person
+          struggles to <span className="font-bold">analyze</span> stock
+          effectively due to the <span className="font-bold">overwhelming</span>{" "}
+          financial reports, complex data, and a lack of clear insights, leading
+          many to default to well-known stocks rather than discovering new,
+          beneficial opportunities.{" "}
+        </p>
+      </div>
+    </div>
+  );
+}
