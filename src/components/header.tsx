a23931b20b06fe783ee423ff85688e809986ecbb
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2025-03-18 15:55:01 -0700

    add hero

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export function Header() {
   return (
-    <div className="flex h-24 w-full items-center justify-between px-16">
+    <div className="flex h-24 w-full items-center justify-between border-b-[0.2px] px-16">
       <div>[Name Here]</div>
       <div className="flex flex-row">
         <Button variant="link" size="lg">
---
+++ src/components/landing/hero.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+export function Hero() {
+  return (
+    <div className="flex h-[calc(100vh-250px)] w-full flex-col items-center justify-center gap-6 p-16 text-center">
+      <h1 className="text-7xl">Custom Portfolio Management</h1>
+      <h2 className="text-muted-foreground px-48 text-2xl">
+        Purpose-built financial AI that transforms complex stock research into
+        clear, actionable insights for smarter investing.
+      </h2>
+      <div className="flex flex-col gap-2">
+        <Button size="lg" className="pl-6">
+          <ArrowRight /> Join Waitlist
+        </Button>
+        <p className="text-sm">Learn more + no obligations</p>
+      </div>
+    </div>
+  );
+}
