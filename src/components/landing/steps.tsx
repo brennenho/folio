eb33c1baa37c32111fb984588c8d175f3dd9f0b6
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2025-03-20 14:41:32 -0700

    remove feature breakdown

--- src/components/landing/steps.tsx
+++ src/components/landing/steps.tsx
@@ -3,34 +3,36 @@ import { Build, Customize, Execute } from "@/components/icons";
 
 export function Steps() {
   return (
-    <div className="my-24 flex w-full flex-col items-center text-center">
-      <div className="flex w-5/6 flex-col items-center gap-16 md:w-3/4">
-        <AnimationWrapper className="text-4xl font-semibold tracking-wide">
-          Get Started in 3 Simple Steps
-        </AnimationWrapper>
-        <div className="flex w-full flex-col gap-16">
-          <Step
-            title="Build"
-            description="Define your investment goals, interests, or themes, and let our
+    <div className="bg-foreground w-full">
+      <div className="bg-background flex w-full flex-col items-center rounded-b-3xl py-24 text-center">
+        <div className="flex w-5/6 flex-col items-center gap-16 md:w-3/4">
+          <AnimationWrapper className="text-4xl font-semibold tracking-wide">
+            Get Started in 3 Simple Steps
+          </AnimationWrapper>
+          <div className="flex w-full flex-col gap-16">
+            <Step
+              title="Build"
+              description="Define your investment goals, interests, or themes, and let our
                 AI curate a diversified selection of stocks. Create a custom ETF
                 tailored to your strategy."
-            icon={<Build className="w-2/3" />}
-          />
+              icon={<Build className="w-2/3" />}
+            />
 
-          <Step
-            title="Customize"
-            description="Refine your portfolio by adjusting allocations, backtesting
+            <Step
+              title="Customize"
+              description="Refine your portfolio by adjusting allocations, backtesting
                 performance, and setting risk preferences to align with your
                 investment approach."
-            icon={<Customize className="w-2/3" />}
-            reverse
-          />
+              icon={<Customize className="w-2/3" />}
+              reverse
+            />
 
-          <Step
-            title="Execute"
-            description="With bank-level security, seamlessly link folio to one or more of your brokerages and bring your portfolio to life."
-            icon={<Execute className="w-2/5" />}
-          />
+            <Step
+              title="Execute"
+              description="With bank-level security, seamlessly link folio to one or more of your brokerages and bring your portfolio to life."
+              icon={<Execute className="w-2/5" />}
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -49,16 +51,16 @@ function Step({
   reverse?: boolean;
 }) {
   return (
-    <AnimationWrapper
+    <div
       className={`flex w-full flex-col ${reverse ? "md:flex-row-reverse" : "md:flex-row"}`}
     >
-      <div className="flex w-full items-center justify-center md:w-1/2">
+      <AnimationWrapper className="flex w-full items-center justify-center md:w-1/2">
         {icon}
-      </div>
-      <div className="flex w-full flex-col justify-center p-8 text-left tracking-tight md:w-1/2 md:p-16">
+      </AnimationWrapper>
+      <AnimationWrapper className="flex w-full flex-col justify-center p-8 text-left tracking-tight md:w-1/2 md:p-16">
         <h1 className="text-2xl font-semibold md:text-4xl">{title}</h1>
         <p>{description}</p>
-      </div>
-    </AnimationWrapper>
+      </AnimationWrapper>
+    </div>
   );
 }
