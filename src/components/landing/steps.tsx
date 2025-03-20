8f5565e93360b379509abf0241063249a7585a7d
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2025-03-20 14:19:45 -0700

    steps section redesign

--- src/components/landing/steps.tsx
+++ src/components/landing/steps.tsx
@@ -1,56 +1,61 @@
 import { AnimationWrapper } from "@/components/animation";
+import { Build, Customize, Execute } from "@/components/icons";
 
 export function Steps() {
   return (
     <div className="my-24 flex w-full flex-col items-center text-center">
-      <div className="flex w-5/6 flex-col items-center gap-8 md:w-3/4">
-        <AnimationWrapper className="text-3xl font-medium tracking-wide">
+      <div className="flex w-5/6 flex-col items-center gap-16 md:w-3/4">
+        <AnimationWrapper className="text-4xl font-semibold tracking-wide">
           Get Started in 3 Simple Steps
         </AnimationWrapper>
-        <div className="md: flex flex-col gap-8 md:flex-row">
+        <div className="flex w-full flex-col gap-16">
           <Step
-            num={1}
-            title="Search"
-            description="Describe your investment goal, interest, or theme and our AI will find relevant, diversified stocks."
+            title="Build"
+            description="Define your investment goals, interests, or themes, and let our
+                AI curate a diversified selection of stocks. Create a custom ETF
+                tailored to your strategy."
+            icon={<Build className="w-1/3" />}
           />
+
           <Step
-            num={2}
             title="Customize"
-            description="Fine-tune your portfolio by adjusting allocations, backtesting performance, and setting risk preferences."
+            description="Refine your portfolio by adjusting allocations, backtesting
+                performance, and setting risk preferences to align with your
+                investment approach."
+            icon={<Customize className="w-1/2" />}
+            reverse
           />
+
           <Step
-            num={3}
             title="Execute"
-            description="Sync with your brokerage account to seamlessly trade and manage your portfolio."
+            description="With bank-level security, seamlessly link folio to one or more of your brokerages and bring your portfolio to life."
+            icon={<Execute className="w-2/5" />}
           />
         </div>
-        <AnimationWrapper className="text-muted-foreground text-xl opacity-50 md:px-16">
-          The platform then continuously analyzes performance, market
-          conditions, and macroeconomic events to suggest re-balancing our new
-          investment opportunities.
-        </AnimationWrapper>
       </div>
     </div>
   );
 }
 
 function Step({
-  num,
   title,
   description,
+  icon,
+  reverse,
 }: {
-  num: number;
   title: string;
   description: string;
+  icon: React.ReactNode;
+  reverse?: boolean;
 }) {
   return (
-    <AnimationWrapper className="flex w-full flex-row items-center gap-6 md:w-1/3 md:flex-col">
-      <div className="bg-primary flex aspect-square h-12 items-center justify-center rounded-full text-xl font-semibold text-white md:h-16">
-        {num}
-      </div>
-      <div className="flex flex-col items-start text-left md:items-center md:text-center">
-        <h2 className="text-xl font-semibold">{title}</h2>
-        <p className="text-muted-foreground opacity-80">{description}</p>
+    <AnimationWrapper
+      className={`flex w-full ${reverse ? "flex-row-reverse" : ""}`}
+    >
+      <div className="flex w-1/2 items-center justify-center">{icon}</div>
+      <div className="flex w-1/2 flex-col justify-center p-16 text-left tracking-tight">
+        <h1 className="text-2xl font-semibold md:text-4xl">{title}</h1>
+        <p>{description}</p>
       </div>
     </AnimationWrapper>
   );
