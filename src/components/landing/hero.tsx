f2c0ee508c3e48c02b309a0f3108112e90f06b4a
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2025-03-24 00:37:30 -0700

    add dash mockup

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -1,14 +1,14 @@
+import { AnimationWrapper } from "@/components/animation";
 import { WaitlistButton } from "@/components/landing/waitlist";
-import { AnimationWrapper } from "../animation";
 
 export function Hero() {
   return (
     <div className="flex h-[calc(100vh-220px)] w-full items-center justify-center text-center">
       <div className="flex w-5/6 flex-col items-center justify-center gap-6 md:w-3/4">
-        <AnimationWrapper className="text-2xl tracking-wider md:text-5xl lg:text-7xl">
+        <AnimationWrapper className="text-3xl font-medium tracking-wider md:text-4xl lg:text-6xl">
           From Idea to Investment - Instantly
         </AnimationWrapper>
-        <AnimationWrapper className="mb-10 max-w-[85%] text-muted-foreground md:text-2xl lg:px-48">
+        <AnimationWrapper className="mb-10 max-w-[85%] text-muted-foreground md:text-2xl">
           folio&rsquo;s financial AI curates custom portfolios based on your
           investment goals, market trends, and real financial data.
         </AnimationWrapper>
