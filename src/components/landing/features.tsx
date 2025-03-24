1cc9d7912dd83cea1f99562a9f9c66759150f823
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2025-03-24 01:19:28 -0700

    fix features layout

--- src/components/landing/features.tsx
+++ src/components/landing/features.tsx
@@ -5,11 +5,11 @@ export function Features() {
   return (
     <div className="flex w-full justify-center bg-foreground py-16 text-background md:py-36">
       <div className="flex w-5/6 flex-col items-center md:w-3/4">
-        <AnimationWrapper className="w-5/6 text-center text-2xl font-medium tracking-wide md:w-3/4 md:text-4xl">
+        <AnimationWrapper className="text-center text-2xl font-medium tracking-wide md:text-4xl">
           Unlock Professional-Level Investing
         </AnimationWrapper>
 
-        <div className="flex flex-col items-center gap-8 pt-16 md:flex-row">
+        <div className="flex w-full flex-col items-center gap-8 pt-16 md:flex-row">
           <AnimationWrapper className="flex w-full flex-row items-center gap-6 text-center md:w-1/3 md:flex-col">
             <Shield className="w-12" />
             <div className="flex flex-col text-left md:text-center">
